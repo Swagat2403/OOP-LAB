c9a3d6754a936ae678631f47f4843cbf3981740d
Swagat2403 <swagatmishra1603@gmail.com>
2023-12-27 13:15:02 +0530

    lab1 q1

---
+++ LAB1/Rectangle.java
@@ -0,0 +1,13 @@
+// 1. Write a Java program to find area and circumference of a rectangle.
+// (Hint: circumference = 2 (length + breadth) ; area= length x breadth).
+
+public class Rectangle{
+    public static void main(String[] args) {
+        int length=5; 
+        int breadth=5;
+        int area= length*breadth;
+        System.out.println("The area is :" +area);
+        
+
+    }
+}
